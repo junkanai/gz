1554b1b226a87631d0a88a2c9c5a0be835a7dd9e
junkanai <kanai.jun.1020@gmail.com>
2022-07-16 12:35:45 +0900

    color update

--- color.py
+++ color.py
@@ -1,17 +1,18 @@
 import sys
+import traceback
 
 red = (255, 0, 0)
-blue = ()
-green = ()
-yellow = ()
-pink = ()
-sky = ()
+blue = (0, 255, 0)
+green = (0, 0, 255)
+yellow = (255, 0, 255)
+pink = (255, 255, 0)
+sky = (0, 255, 255)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 def gray(value):
-    if 0 <= value < 256: return (value, value, value)
-    print(f"value should be 0 to 255, not {value}!")
-    sys.exit()
-
-
+    try:
+        if 0 <= value < 256: return (value, value, value)
+        raise ValueError
+    except ValueError as e:
+        raise ValueError(f"gray should be 0 to 255, not {value}!")
