46824973f585ca73b04a78903566cfee5c773104
junkanai <kanai.jun.1020@gmail.com>
2022-07-16 13:17:44 +0900

    image update

--- draw/image.py
+++ draw/image.py
@@ -2,5 +2,14 @@ import pygame
 from .gzobject import GzObject
 
 class Image(GzObject):
-    pass
+    def __init__(self, path, size=(50, 50), priority=90):
+        super().__init__(w=size[0], h=size[1], priority=priority)
+        self.__original_image = pygame.image.load(path)
+        self.__update()
 
+    def __update(self):
+        self.__image = pygame.transform.scale(self.__original_image, (self.w, self.h))
+
+    def draw(self, screen, dx=0, dy=0):
+        if self.visible:
+            screen.blit(self.__image, (self.x+dx, self.y+dy))
